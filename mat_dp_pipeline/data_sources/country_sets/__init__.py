12670c1ebc683cff412c2adc394f6e61e3ce15c5
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-20 13:06:59 +0000

    Rearranged country set location
    Tweaked interface for country codes to use classvar directly

---
+++ mat_dp_pipeline/data_sources/country_sets/__init__.py
@@ -0,0 +1,5 @@
+from .country_set import CountrySet
+from .create_country_to_path import CountrySets
+from .source_with_countries import SourceWithCountries
+
+__all__ = ["CountrySet", "CountrySets", "SourceWithCountries"]
