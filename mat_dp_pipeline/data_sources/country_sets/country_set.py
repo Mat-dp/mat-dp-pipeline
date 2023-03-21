ba71e3a5ae9c40d1320ceae3fd1da0b09d00ba76
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-21 14:18:57 +0000

    First edition of identifier test

--- mat_dp_pipeline/data_sources/country_sets/country_set.py
+++ mat_dp_pipeline/data_sources/country_sets/country_set.py
@@ -1,46 +1,53 @@
 from dataclasses import dataclass, field
-from typing import ClassVar
 
 import pandas as pd
+
+from .identifier import Identifier
 
 __all__ = ["CountrySet"]
 
 
+def get_none():
+    return None
+
+
 @dataclass
 class CountrySet:
-    country_code_df: ClassVar[pd.DataFrame] = pd.read_csv("./country_codes.csv")
     countries: list[str] = field(default_factory=list)
     regions: list[str] = field(default_factory=list)
     sub_regions: list[str] = field(default_factory=list)
     intermediate_regions: list[str] = field(default_factory=list)
     custom_countries: list[str] = field(default_factory=list)
-    _all_countries: list[str] = field(init=False, repr=False)
+    _all_countries: dict[Identifier, list[str]] = field(
+        default_factory=dict, init=False, repr=False
+    )
 
-    @property
-    def all_countries(self):
-        if not hasattr(self, "_all_countries"):
+    def all_countries(self, country_code_df: pd.DataFrame, identifier: Identifier):
+        if identifier not in self._all_countries:
+            identifier_value: str = identifier.value
             total_countries = []
-            available_countries = list(self.country_code_df["name"])
+            available_countries = list(country_code_df[identifier_value])
+            print(available_countries)
             for country in self.countries:
                 if country not in available_countries:
                     raise ValueError(f"Country {country} not found")
                 total_countries.append(country)
-
             for region in self.regions:
                 rel_countries = list(
-                    self.country_code_df[self.country_code_df["region"] == region][
-                        "name"
+                    country_code_df[country_code_df["region"] == region][
+                        identifier_value
                     ]
                 )
+
                 if len(rel_countries) == 0:
                     raise ValueError(f"Region {region} not found")
                 total_countries += rel_countries
 
             for sub_region in self.sub_regions:
                 rel_countries = list(
-                    self.country_code_df[
-                        self.country_code_df["sub-region"] == sub_region
-                    ]["name"]
+                    country_code_df[country_code_df["sub-region"] == sub_region][
+                        identifier_value
+                    ]
                 )
                 if len(rel_countries) == 0:
                     raise ValueError(f"Sub region {sub_region} not found")
@@ -48,10 +55,9 @@ class CountrySet:
 
             for intermediate_region in self.intermediate_regions:
                 rel_countries = list(
-                    self.country_code_df[
-                        self.country_code_df["intermediate-region"]
-                        == intermediate_region
-                    ]["name"]
+                    country_code_df[
+                        country_code_df["intermediate-region"] == intermediate_region
+                    ][identifier_value]
                 )
                 if len(rel_countries) == 0:
                     raise ValueError(
@@ -61,6 +67,7 @@ class CountrySet:
 
             for custom_country in self.custom_countries:
                 total_countries.append(custom_country)
-
-            self._all_countries = list(set(total_countries))
-        return self._all_countries
+            self._all_countries[identifier] = list(set(total_countries))
+            return self._all_countries[identifier]
+        else:
+            return self._all_countries[identifier]
--- mat_dp_pipeline/data_sources/country_sets/create_country_to_path.py
+++ mat_dp_pipeline/data_sources/country_sets/create_country_to_path.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 
+import pandas as pd
+
 from .country_set import CountrySet
+from .identifier import Identifier
 
 __all__ = ["CountrySets", "create_country_to_path"]
 
 CountrySets = dict[str, "CountrySet | CountrySets"]
 
 
-def create_country_to_path(country_sets: CountrySets) -> dict[str, Path]:
+def create_country_to_path(
+    country_sets: CountrySets, country_code_df: pd.DataFrame, identifier: Identifier
+) -> dict[str, Path]:
     final_dict: dict[str, list[str]] = {}
 
     def dfs(country_sets: CountrySets, prefix: list[str]):
         for k, v in country_sets.items():
             if isinstance(v, CountrySet):
-                all_countries = v.all_countries
-                for country in all_countries:
+                for country in v.all_countries(country_code_df, identifier):
                     if country in final_dict:
                         raise ValueError(
                             f"Overlapped disjoint set detected for {country}"
