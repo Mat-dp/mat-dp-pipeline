5d6d376f61f4bc561350f40afe89bd86be511fbe
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-21 17:02:42 +0000

    Fixed path name issue

--- mat_dp_pipeline/data_sources/country_sets/country_set.py
+++ mat_dp_pipeline/data_sources/country_sets/country_set.py
@@ -22,7 +22,7 @@ class CountrySet:
     sub_regions: list[str] = field(default_factory=list)
     intermediate_regions: list[str] = field(default_factory=list)
     custom_countries: list[CustomCountry] = field(default_factory=list)
-    _all_countries: dict[Identifier, list[str]] = field(
+    _all_countries: dict[Identifier, list[tuple[str, str]]] = field(
         default_factory=dict, init=False, repr=False
     )
 
@@ -30,11 +30,14 @@ class CountrySet:
         if identifier not in self._all_countries:
             identifier_value: str = identifier.value
             total_countries = []
-            available_countries = list(country_code_df[identifier_value])
+            country_names = []
+            available_countries = list(country_code_df[Identifier.country_name.value])
             for country in self.countries:
                 if country not in available_countries:
                     raise ValueError(f"Country {country} not found")
                 total_countries.append(country)
+                country_names.append(country)
+
             for region in self.regions:
                 rel_countries = list(
                     country_code_df[country_code_df["region"] == region][
@@ -46,6 +49,13 @@ class CountrySet:
                     raise ValueError(f"Region {region} not found")
                 total_countries += rel_countries
 
+                rel_names = list(
+                    country_code_df[country_code_df["region"] == region][
+                        Identifier.country_name.value
+                    ]
+                )
+                country_names += rel_names
+
             for sub_region in self.sub_regions:
                 rel_countries = list(
                     country_code_df[country_code_df["sub-region"] == sub_region][
@@ -55,6 +65,13 @@ class CountrySet:
                 if len(rel_countries) == 0:
                     raise ValueError(f"Sub region {sub_region} not found")
                 total_countries += rel_countries
+
+                rel_names = list(
+                    country_code_df[country_code_df["sub-region"] == sub_region][
+                        Identifier.country_name.value
+                    ]
+                )
+                country_names += rel_names
 
             for intermediate_region in self.intermediate_regions:
                 rel_countries = list(
@@ -68,21 +85,33 @@ class CountrySet:
                     )
                 total_countries += rel_countries
 
+                rel_names = list(
+                    country_code_df[
+                        country_code_df["intermediate-region"] == intermediate_region
+                    ][Identifier.country_name.value]
+                )
+                country_names += rel_names
+
             for custom_country in self.custom_countries:
                 if identifier == Identifier.country_name:
                     total_countries.append(custom_country.name)
+                    country_names.append(custom_country.name)
                 elif (
                     identifier == Identifier.alpha_2
                     and custom_country.alpha_2 is not None
                 ):
                     total_countries.append(custom_country.alpha_2)
+                    country_names.append(custom_country.name)
                 elif (
                     identifier == Identifier.alpha_3
                     and custom_country.alpha_3 is not None
                 ):
                     total_countries.append(custom_country.alpha_3)
+                    country_names.append(custom_country.name)
 
-            self._all_countries[identifier] = list(set(total_countries))
+            self._all_countries[identifier] = list(
+                set(zip(total_countries, country_names))
+            )
             return self._all_countries[identifier]
         else:
             return self._all_countries[identifier]
