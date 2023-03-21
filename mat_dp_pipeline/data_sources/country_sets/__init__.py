4a5ee62f982b019034f0f4cd6c00d51446e87709
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-21 18:26:03 +0000

    Addressed review about __all__

--- mat_dp_pipeline/data_sources/country_sets/__init__.py
+++ mat_dp_pipeline/data_sources/country_sets/__init__.py
@@ -1,5 +1,5 @@
+__all__ = ["CountrySet", "CountrySets", "SourceWithCountries", "CustomCountry"]
+
 from .country_set import CountrySet, CustomCountry
 from .create_country_to_path import CountrySets
 from .source_with_countries import SourceWithCountries
-
-__all__ = ["CountrySet", "CountrySets", "SourceWithCountries", "CustomCountry"]
