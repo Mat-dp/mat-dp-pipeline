c1ab6cb9bfacc35e020368471c32a6f7d0ec04a8
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-21 17:02:42 +0000

    Fixed path name issue

--- mat_dp_pipeline/data_sources/country_sets/create_country_to_path.py
+++ mat_dp_pipeline/data_sources/country_sets/create_country_to_path.py
@@ -18,16 +18,18 @@ def create_country_to_path(
     def dfs(country_sets: CountrySets, prefix: list[str]):
         for k, v in country_sets.items():
             if isinstance(v, CountrySet):
-                for country in v.all_countries(country_code_df, identifier):
+                for country, country_name in v.all_countries(
+                    country_code_df, identifier
+                ):
                     if country in final_dict:
                         raise ValueError(
                             f"Overlapped disjoint set detected for {country} location is already {final_dict[country]}"
                         )
                     else:
                         if k == "__root__":
-                            final_dict[country] = prefix
+                            final_dict[country] = prefix + [country_name]
                         else:
-                            final_dict[country] = prefix + [k]
+                            final_dict[country] = prefix + [k, country_name]
             else:
                 dfs(v, prefix=prefix + [k])
 
