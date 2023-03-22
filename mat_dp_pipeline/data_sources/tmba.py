004a1164a9204a08f337bc9567ba6decb6e93415
Marcin Krupowicz <ma@rcin.me>
2023-03-22 16:35:32 +0000

    Integrated Assesment Model data source

--- mat_dp_pipeline/data_sources/tmba.py
+++ mat_dp_pipeline/data_sources/tmba.py
@@ -1,4 +1,3 @@
-import logging
 from pathlib import Path
 from typing import ClassVar, Final
 
@@ -10,12 +9,14 @@ from mat_dp_pipeline.data_sources.country_sets.source_with_countries import (
     SourceWithCountries,
 )
 
+from .common import map_technologies
 from .tech_map import TechMap, TechMapTypes, create_tech_map
 
 
 class TMBATargetsSource(TargetsSource):
     _targets_csv: Path
     _targets_parameters: list[str]
+    _country_to_path: dict[str, Path]
     _grouping: Final[tuple[str, ...]] = ("country", "parameter")
     _tech_map: ClassVar[TechMap] = create_tech_map(TechMapTypes.TMBA)
 
@@ -37,32 +38,16 @@ class TMBATargetsSource(TargetsSource):
         targets = pd.read_csv(self._targets_csv)
         targets = targets[targets["parameter"].isin(self._targets_parameters)]
         targets = targets.drop(columns=[targets.columns[0], "scenario"]).dropna()
-
-        tech_tuples = targets["variable"].map(self._tech_map)
-        if unmapped_variables := set(
-            targets.loc[tech_tuples[tech_tuples.isna()].index, "variable"]
-        ):
-            logging.warning(
-                f"The following variables don't have a corresponding mapping in the Tech Map: {unmapped_variables}. Ignoring them."
-            )
-            tech_tuples = tech_tuples.dropna()
-
-        targets.pop("variable")  # no longer needed
-        techs = pd.DataFrame.from_records(
-            tech_tuples.to_list(),
-            index=tech_tuples.index,
-            columns=["Category", "Specific"],
-        )
-        targets = targets.join(techs, how="inner")
+        targets = map_technologies(targets, "variable", self._tech_map)
 
         grouping = list(self._grouping)  # list needed
         for key, targets_frame in targets.groupby(grouping):
             if key[0] == "NM":
                 # Special case for namibia, as we override alpha 2
-                new_key = "NA"
+                country = "NA"
             else:
-                new_key = key[0]
-            path = (self._country_to_path[new_key],) + key[1:]
+                country = key[0]
+            path = (self._country_to_path[country],) + key[1:]
             path = Path(*path)
             location_dir = output_dir / path.relative_to("/")
             location_dir.mkdir(exist_ok=True, parents=True)
