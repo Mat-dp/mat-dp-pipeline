f60566f6e81edcb64aa594f6bfd9320b6b526a54
mark-todd <markpeter.todd@hotmail.co.uk>
2023-03-23 13:24:34 +0000

    Fixed identifier location error

--- mat_dp_pipeline/data_sources/country_sets/__init__.py
+++ mat_dp_pipeline/data_sources/country_sets/__init__.py
@@ -1,5 +1,12 @@
-__all__ = ["CountrySet", "CountrySets", "SourceWithCountries", "CustomCountry"]
+__all__ = [
+    "CountrySet",
+    "CountrySets",
+    "SourceWithCountries",
+    "CustomCountry",
+    "Identifier",
+]
 
 from .country_set import CountrySet, CustomCountry
 from .create_country_to_path import CountrySets
+from .identifier import Identifier
 from .source_with_countries import SourceWithCountries
